e4e7d5b613b8c1345e13ff49dd7ca22b1df2bc7f
Rio Nyx <58178550+Rio-Nyx@users.noreply.github.com>
2022-11-28 17:48:27 +0530

    Update complete-survey.js

--- etlab/complete-survey.js
+++ etlab/complete-survey.js
@@ -1,9 +1,10 @@
 // script to complete the survey published in etlab
 
 let option = 2;
+let num_options = 5;
 $("form#survey-form :input[type=radio]").each(function(){ 
  let input = $(this); 
-    if(input.val()%5==option){
+    if(input.val()%num_options==option){
         input.attr('checked', 'true');
     }
 });
